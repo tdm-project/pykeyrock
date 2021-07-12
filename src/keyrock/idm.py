09a82590043d49175a795b0d862f83a401412aec
Massimo Gaggero <massimo.gaggero@crs4.it>
2021-07-12 17:28:24 +0200

    - implements Organization's CRUD operations; - adds testcases for Organization's CRUD operations.

---
+++ src/keyrock/idm.py
@@ -0,0 +1,251 @@
+#  Copyright 2021, CRS4 - Center for Advanced Studies, Research and Development
+#  in Sardinia
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from .models import IDMOrganization
+import dateutil.parser
+import inspect
+import json
+import logging
+import requests
+from http.client import responses
+
+
+def get_auth_token(host: str, port: int, user: str, password: str):
+    url = f"http://{host}:{port}/v1/auth/tokens"
+    payload = {
+        "name": user,
+        "password": password
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    response = requests.request(
+        "POST", url, headers=headers, data=json.dumps(payload))
+    response.raise_for_status()
+
+    expires = dateutil.parser.isoparse(response.json()['token']['expires_at'])
+    expires = expires.strftime("%c")
+
+    token = response.headers['X-Subject-Token']
+
+    return(token, expires)
+
+
+class IDMManager(object):
+    def __init__(self, host: str, port: int, auth_token: str):
+        self._host = host
+        self._port = port
+        self._idm_url = f"http://{host}:{port}"
+        self._auth_token = auth_token
+
+        self._logger = logging.getLogger('keyrock.IDMManager')
+        self._logger.debug(
+            'creating an instance of IDMManager (%s, %s)',
+            self._idm_url, self._auth_token)
+
+    def _log_response(self, response):
+        _func_name = inspect.stack()[1].function
+        _http_ver = ('HTTP/1.1' if
+                     response.raw.version == 11 else 'HTTP/1.0')
+        _level = logging.DEBUG if response.status_code < 400 else logging.ERROR
+
+        if response.status_code < 400:
+            _reason = ""
+        elif 'error' in response.json():
+            _reason = f"\"{response.json()['error']['message']}\""
+        else:
+            _reason = f"\"{response.json()}\""
+
+        self._logger.log(
+            _level,
+            (f'{_func_name}() - '
+             f'{self._idm_url} "{response.request.method} '
+             f'{response.request.path_url} {_http_ver}" '
+             f'{response.status_code} "{responses[response.status_code]}": '
+             f'{_reason}'))
+
+    def get_oauth2_token(self, user: str, password: str,
+                         application_secret: str, permanent: bool):
+        url = f"{self._idm_url}/oauth2/token"
+        payload = {
+            "username": user,
+            "password": password,
+            "grant_type": "password"
+        }
+
+        if permanent:
+            payload.update({"scope": "permanent"})
+
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': f'Basic {application_secret}',
+            'Accept': 'application/json'
+        }
+
+        response = requests.request(
+            "POST", url, headers=headers, data=payload)
+        self._log_response(response)
+        response.raise_for_status()
+
+        return response.json()['access_token']
+
+    ###########################################################################
+    # ORGANIZATIONS section
+    ###########################################################################
+    def get_organization(self, organization_id: str):
+        """
+        Retrieves information about the organizations with the given id,
+        if exist.
+
+        Args:
+            organization_id (str): The organization id.
+
+            Returns:
+                - an IDMOrganization object with the organization details
+                - None if the organization does not exist.
+        """
+        url = f"{self._idm_url}/v1/organizations/{organization_id}"
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-token': self._auth_token
+        }
+        response = requests.request("GET", url, headers=headers)
+        self._log_response(response)
+
+        if response.status_code == requests.codes.ok:
+            _organization = IDMOrganization(
+                org_dict=response.json()['organization'])
+        else:
+            _organization = None
+
+        return _organization
+
+    def list_organizations(self):
+        """
+        Returns a list of all the organizations in the IDM.
+
+        Returns:
+            - a list of IDMOrganization objects.
+        """
+        url = f"{self._idm_url}/v1/organizations"
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-token': self._auth_token
+        }
+        response = requests.request("GET", url, headers=headers)
+        self._log_response(response)
+
+        _org_list = list()
+        if response.status_code == requests.codes.ok:
+            for _org in response.json()['organizations']:
+                _org_list.append(
+                    IDMOrganization(org_dict=_org['Organization']))
+
+        return _org_list
+
+    def delete_organization(self, organization_id: str):
+        """
+        Deletes the organizations with the given id, if exist.
+        WARNING: it does not asks for confirmation!
+
+        Args:
+            organization_id (str): The organization id.
+
+        Raises:
+            HTTPError if the operation was not successfull.
+
+        Reference:
+            https://github.com/FIWARE/tutorials.Identity-Management#delete-an-organization
+        """
+        url = f"{self._idm_url}/v1/organizations/{organization_id}"
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-token': self._auth_token
+        }
+        response = requests.request("DELETE", url, headers=headers)
+        self._log_response(response)
+
+        response.raise_for_status()
+
+    def get_organizations_by_name(self, org_name: str):
+        """
+        Retrieves information about the organizations with the given name,
+        if exist. Warning: more than one organization can exist with the same
+        name.
+
+        Args:
+            organization_name (str): The organization name.
+
+            Returns:
+                - a list of IDMOrganization objects.
+        """
+        orgs = self.list_organizations()
+        org_list = list()
+
+        for _org in orgs:
+            if _org.name == org_name:
+                org_list.append(_org)
+
+        if len(org_list) > 1:
+            self._logger.warning(
+                'multiple organization with the name "%s" found',
+                org_name)
+
+        return org_list
+
+    def create_organization(self, name, description: str = None):
+        """
+        Creates a new organization.
+        Warning: more than one organization can be created with the same!
+
+        Args:
+            name (str): The organization's name.
+            description (str): The organization's description.
+
+            Returns:
+                - the IDMOrganization object.
+        """
+        url = f"{self._idm_url}/v1/organizations"
+
+        # XXX: Due to a bug (?) in Keyrock an empty-string description is
+        # refused with a 500 error code. None or a missing description key are
+        # accepted but disrupt the database.
+        payload = {
+            "organization": {
+                "name": name,
+                "description":
+                    description or f"This is the {name} organization"
+            }
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-token': self._auth_token
+        }
+
+        response = requests.request(
+            "POST", url, headers=headers, data=json.dumps(payload))
+        self._log_response(response)
+        response.raise_for_status()
+
+        self._logger.info(
+            "IDM organizzation \"%s\" created", name)
+        return IDMOrganization(
+            name, response.json()['organization'])
+
+    def update_organization(self, organization_id: str):
+        raise NotImplementedError()
---
+++ tests/test_organization.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+#
+#  Copyright 2021 CRS4 - Center for Advanced Studies, Research and Development
+#  in Sardinia
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+"""
+This module tests CRUD operations on Keyrock Application:
+    *
+"""
+
+# import logging
+import uuid
+import unittest
+from lorem_text import lorem
+
+# from keyrock import IDMApplication
+# from keyrock import IDMProxy
+from keyrock import IDMManager, get_auth_token
+from requests.exceptions import HTTPError
+
+
+KEYROCK_ORG = "Test Org"
+KEYROCK_APP = "Test App"
+
+
+class TestOrganization(unittest.TestCase):
+    """
+    Tests Keyrock Organizations CRUD operations.
+    """
+    def setUp(self):
+        self.keyrock_host = "localhost"
+        self.keyrock_port = 3005
+        self.keyrock_admin = "admin@test.com"
+        self.keyrock_passw = "1234"
+        self.auth_token, _ = get_auth_token(
+            self.keyrock_host, self.keyrock_port, self.keyrock_admin,
+            self.keyrock_passw)
+
+        # logging.getLogger('keyrock.IDMManager').setLevel(logging.DEBUG)
+
+        self._im = IDMManager(
+            self.keyrock_host, self.keyrock_port, self.auth_token)
+
+    def random_org_name(self, prefix='pykeyrock unittest', words=2):
+        return f"{prefix} {lorem.words(words)}".capitalize()
+
+    def test_create_organization(self):
+        """
+        """
+        _org_name = self.random_org_name()
+        self._im.create_organization(_org_name)
+
+        _orgs = self._im.get_organizations_by_name(_org_name)
+        self.assertNotEqual(len(_orgs), 0, "Organization not created")
+        self.assertEqual(len(_orgs), 1, "More than one organizations created")
+
+    def test_create_organization_empty_description(self):
+        """
+        """
+        _org_name = self.random_org_name()
+        _org_desc = f"This is the {_org_name} organization"
+        _org = self._im.create_organization(_org_name)
+
+        self.assertEqual(_org.name, _org_name, "Wrong name")
+        self.assertEqual(_org.description, _org_desc, "Wrong description")
+
+    def test_create_organization_with_description(self):
+        """
+        """
+        _org_name = self.random_org_name()
+        _description = lorem.words(5).capitalize() + '.'
+
+        _org = self._im.create_organization(_org_name, _description)
+
+        self.assertEqual(_org.name, _org_name, "Wrong name")
+        self.assertEqual(_org.description, _description, "Wrong description")
+
+    def test_create_duplicated_organization(self):
+        """
+        """
+        _org_ids = list()
+        _org_name = self.random_org_name()
+
+        _org = self._im.create_organization(_org_name)
+        _org_ids.append(_org.id)
+
+        _org = self._im.create_organization(_org_name)
+        _org_ids.append(_org.id)
+
+        _orgs = self._im.get_organizations_by_name(_org_name)
+        self.assertEqual(len(_orgs), 2, "Duplicated organizations not created")
+
+    def test_get_organization(self):
+        """
+        """
+        _org_name = self.random_org_name()
+        _description = lorem.words(5).capitalize() + '.'
+
+        _org = self._im.create_organization(_org_name, _description)
+
+        _res = self._im.get_organization(_org.id)
+
+        self.assertEqual(_org.name, _res.name, "Wrong name")
+        self.assertEqual(_org.description, _res.description,
+                         "Wrong description")
+
+        _res = self._im.get_organization(uuid.uuid4())
+        self.assertEqual(_res, None, "Returns not None object")
+
+    def test_delete_organization(self):
+        _org_name = self.random_org_name()
+        _description = lorem.words(5).capitalize() + '.'
+        _org = self._im.create_organization(_org_name, _description)
+
+        _res = self._im.get_organization(_org.id)
+
+        self.assertEqual(_org.name, _res.name, "Wrong name")
+        self.assertEqual(_org.description, _res.description,
+                         "Wrong description")
+
+        _res = self._im.delete_organization(_org.id)
+        _res = self._im.get_organization(_org.id)
+        self.assertEqual(_res, None, 'Organization not deleted')
+
+    def test_delete_not_existing_organization(self):
+        """
+        """
+        _org_id = uuid.uuid4()
+        with self.assertRaises(
+                HTTPError,
+                msg="Not raising error on not existing organization"):
+            self._im.delete_organization(_org_id)
+
+    def test_update_organization(self):
+        """
+        """
+        _org_id = uuid.uuid4()
+        with self.assertRaises(
+                NotImplementedError, msg="Not implemented method exists?"):
+            self._im.update_organization(_org_id)
+
+    def tearDown(self):
+        _orgs = self._im.list_organizations()
+        for _org in _orgs:
+            if _org.name.startswith('pykeyrock unittest'.capitalize()):
+                self._im.delete_organization(_org.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
